e7a231f7aa73202818384843f9a826b1ca7f9749
Anthony Parker <anthonyparker121@gmail.com>
2018-04-20 16:14:27 +0100

    Remove example classes

--- site/web/app/mu-plugins/buzzeasy/modules.php
+++ site/web/app/mu-plugins/buzzeasy/modules.php
@@ -1,13 +1,7 @@
 <?php
 
 return [
-    \Buzzeasy\App\Modules\AjaxEndpoints::class => [
-        \Buzzeasy\App\AjaxEndpoints\Post::class,
-        \Buzzeasy\App\AjaxEndpoints\Page::class,
-    ],
-
     \Buzzeasy\App\Modules\PostTypes::class => [
         \Buzzeasy\App\PostTypes\Example::class,
-        \Buzzeasy\App\PostTypes\ConfigExample::class,
     ],
 ];
