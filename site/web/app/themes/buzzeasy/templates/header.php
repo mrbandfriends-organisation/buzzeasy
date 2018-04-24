7495e9a9cd254e190756570073b6f8a8cdf69e55
Anthony Parker <anthonyparker121@gmail.com>
2018-04-24 17:10:43 +0100

    Pad nav

--- site/web/app/themes/buzzeasy/templates/header.php
+++ site/web/app/themes/buzzeasy/templates/header.php
@@ -5,23 +5,25 @@
 <header class="banner">
     <a href="#main-content" class="skip-link">Skip to content</a>
 
-    <div class="container">
-    <?php
-        echo Utils\ob_load_template_part('templates/partials/site-logo.php');
-    ?>
+    <div class="banner__inner">
+        <div class="container">
+        <?php
+            echo Utils\ob_load_template_part('templates/partials/site-logo.php');
+        ?>
 
-    <?php
-        $banner_nav_args = array(
-            'modifier' => 'banner'
-        );
+        <?php
+            $banner_nav_args = array(
+                'modifier' => 'banner'
+            );
 
-        echo Utils\ob_load_template_part('templates/menus/primary-nav.php', $banner_nav_args);
-    ?>
+            echo Utils\ob_load_template_part('templates/menus/primary-nav.php', $banner_nav_args);
+        ?>
 
-    <button id="menu-button" type="button" class="offcanvas-toggle offcanvas-toggle--open tcon tcon-menu--xcross js-offcanvas-toggle" aria-label="toggle menu" aria-expanded="false" aria-controls="menu">
-            <span class="offcanvas-toggle__text"><span class="vh">Open menu</span></span>
-            <span class="offcanvas-toggle__lines tcon-menu__lines" aria-hidden="true"></span>
-    </button>
+        <button id="menu-button" type="button" class="offcanvas-toggle offcanvas-toggle--open tcon tcon-menu--xcross js-offcanvas-toggle" aria-label="toggle menu" aria-expanded="false" aria-controls="menu">
+                <span class="offcanvas-toggle__text"><span class="vh">Open menu</span></span>
+                <span class="offcanvas-toggle__lines tcon-menu__lines" aria-hidden="true"></span>
+        </button>
 
+        </div>
     </div>
 </header>
