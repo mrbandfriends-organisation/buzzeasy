d6859fa4173bb54f85ad59ac2010cdbfba05f462
Anthony Parker <anthonyparker121@gmail.com>
2018-04-24 12:42:02 +0100

    Initial prototype of homepage

--- site/web/app/themes/buzzeasy/template-home.php
+++ site/web/app/themes/buzzeasy/template-home.php
@@ -4,10 +4,90 @@
  * Template Name: Homepage Template
  */
 
-	use Roots\Sage\Utils;
-    use Buzzeasy\App\Utilities\Post;
-    use Buzzeasy\App\Utilities\ValueCollection;
-    $post = new Post;
-
 ?>
 
+<section class="page-hero lazyload text-center">
+	<div class="container">
+		<div class="page-hero__inner">
+			<header>
+				<h1 class="page-hero__heading heading--alpha">
+					Homepage Heading
+				</h1>
+			</header>
+			
+			<h2 class="heading--charlie">
+				Subheading
+			</h2>
+		</div>
+	</div>
+</section>
+
+<section class="customer-journey band">
+	<div class="container">
+		<h2 class="text-center">Customer journey</h2>
+		<img src="<?=get_template_directory_uri() ?>/assets/images/journey.png" alt="">
+	</div>
+</section>
+
+<section class="services band band--off-white">
+	<div class="container">
+		<h2 class="text-center">Services</h2>
+
+		<div class="grid">
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x150" alt="" class="service-icon"><h3>Service</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x150" alt="" class="service-icon"><h3>Service</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x150" alt="" class="service-icon"><h3>Service</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x150" alt="" class="service-icon"><h3>Service</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x150" alt="" class="service-icon"><h3>Service</h3></div>
+		</div>
+	</div>
+</section>
+
+<section class="cta-banner band">
+	<div class="container text-center">
+		<a href="/contact-us/" class="btn btn-primary">Free trial</a>
+	</div>
+</section>
+
+<section class="benefits band band--off-white"><div class="container">
+    <div class="grid">
+		<div class="gc s1-1 l1-2">
+			<h2>Contact Centre Benefits</h2>
+			<ul>
+				<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+				<li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+				<li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
+			</ul>
+		</div>
+		<div class="gc s1-1 l1-2">
+			<h2>Marketing Benefits</h2>
+			<ul>
+				<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+				<li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+				<li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
+			</ul>
+		</div>
+	</div>
+</section>
+
+<section class="endorsements band">
+	<div class="container">
+		<h2 class="text-center">
+			Endorsements
+		</h2>
+
+		<div class="grid">
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x50" alt="" class="endorsement-icon"><h3>Company</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x50" alt="" class="endorsement-icon"><h3>Company</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x50" alt="" class="endorsement-icon"><h3>Company</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x50" alt="" class="endorsement-icon"><h3>Company</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x50" alt="" class="endorsement-icon"><h3>Company</h3></div>
+		</div>
+	</div>
+</section>
+
+<section class="cta-banner band band--off-white">
+	<div class="container text-center">
+		<a href="/contact-us/" class="btn btn-primary">Call us</a>
+	</div>
+</section>
--- site/web/app/themes/buzzeasy/templates/footer.php
+++ site/web/app/themes/buzzeasy/templates/footer.php
@@ -1,5 +1,32 @@
-<footer class="content-info">
+<?php
+
+    use Roots\Sage\Utils;
+    use Roots\Sage\Extras;
+
+?>
+
+<footer class="footer content-info band">
+  <h2 class="vh">Buzzeasy Footer Section</h2>
   <div class="container">
+    <div class="grid">
+        <div class="gc t1-1 s1-4">
+            <?= Utils\ob_load_template_part('templates/partials/site-logo.php'); ?>
+        </div>
+        <div class="gc t1-1 s1-4">
+            <div class="footer__left-column">
+                <h3 class="vh heading--white">Buzzeasy Footer Navigation</h3>
+                
+                <div class="social-menu">
+                    <h3 class="vh heading--white">Buzzeasy Social Menu</h3>
+                    <p class="social-menu__text text--uppercase">Find us on Social</p>
+                    <?= Utils\ob_load_template_part('templates/partials/shared/social-menu.php'); ?>
+                </div>
+            </div>
+        </div>
+        <div class="gc t1-1 s2-4">
+            © Buzzeasy <?php echo date("Y"); ?>. All Rights Reserved. <br /> <a class="text--underlined" href="https://www.mrbandfriends.co.uk" <?= Extras\link_open_new_tab_attrs(); ?>>Site by Mr B & Friends</a></p>
+        </div>
+    </div>
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
