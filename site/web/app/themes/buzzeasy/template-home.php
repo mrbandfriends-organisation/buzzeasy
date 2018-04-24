907e3a310df41c3ca3a0c0560b4ede522512aa25
Anthony Parker <anthonyparker121@gmail.com>
2018-04-24 10:48:20 +0100

    Create Homepage template

---
+++ site/web/app/themes/buzzeasy/template-home.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Template Name: Homepage Template
+ */
+
+	use Roots\Sage\Utils;
+    use Buzzeasy\App\Utilities\Post;
+    use Buzzeasy\App\Utilities\ValueCollection;
+    $post = new Post;
+
+?>
+
