7d6afd23820f3fb29050c411b1fc7b9a32bb39e2
Anthony Parker <anthonyparker121@gmail.com>
2018-04-25 15:46:33 +0100

    Update favicon to new primary colour

--- site/web/app/themes/buzzeasy/templates/core/meta-icons.php
+++ site/web/app/themes/buzzeasy/templates/core/meta-icons.php
@@ -1,11 +1,10 @@
 <?php $stylesheet_path = get_stylesheet_directory_uri(); ?>
-
 <link rel="apple-touch-icon" sizes="180x180" href="<?php echo ($stylesheet_path); ?>/assets/images/favicons/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="<?php echo ($stylesheet_path); ?>/assets/images/favicons/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="<?php echo ($stylesheet_path); ?>/assets/images/favicons/favicon-16x16.png">
 <link rel="manifest" href="<?php echo ($stylesheet_path); ?>/assets/images/favicons/site.webmanifest">
-<link rel="mask-icon" href="<?php echo ($stylesheet_path); ?>/assets/images/favicons/safari-pinned-tab.svg" color="#eb0028">
+<link rel="mask-icon" href="<?php echo ($stylesheet_path); ?>/assets/images/favicons/safari-pinned-tab.svg" color="#2f7de1">
 <link rel="shortcut icon" href="<?php echo ($stylesheet_path); ?>/assets/images/favicons/favicon.ico">
-<meta name="msapplication-TileColor" content="#eb0028">
+<meta name="msapplication-TileColor" content="#2d89ef">
 <meta name="msapplication-config" content="<?php echo ($stylesheet_path); ?>/assets/images/favicons/browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
