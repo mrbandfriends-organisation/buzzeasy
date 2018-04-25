495183e5a37ab6835969031e918eb35229d1a525
Anthony Parker <anthonyparker121@gmail.com>
2018-04-25 12:30:05 +0100

    Flesh out Services page

--- site/web/app/themes/buzzeasy/template-services.php
+++ site/web/app/themes/buzzeasy/template-services.php
@@ -22,42 +22,113 @@
 	</div>
 </section>
 
-<section class="customer-journey band">
+<section class="service band band--off-white">
 	<div class="container">
-		<h2>Vision/brand</h2>
-	</div>
-</section>
-
-<section class="benefits band band--off-white"><div class="container">
-    <div class="grid">
-		<div class="gc s1-1 l1-2">
-			<h2>Team</h2>
-			<ul>
-				<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-				<li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-				<li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
-			</ul>
+		<div class="grid">
+			<div class="gc s1-1 m2-3">
+				<h2>Service Name</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</p>
+				<ul>
+					<li>Detail about the product</li>
+					<li>Detail about the product</li>
+					<li>Detail about the product</li>
+				</ul>
+			</div>
+			<div class="gc s1-1 m1-3">
+				<h3>Pricing</h3>
+				<ul style="list-style: none">
+					<li>XX users - £YY/month</li>
+					<li>XX users - £YY/month</li>
+					<li>XX users - £YY/month</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </section>
 
-<section class="benefits band"><div class="container">
-    <div class="grid">
-		<div class="gc s1-1 l1-2">
+<section class="service band">
+	<div class="container">
+		<div class="grid">
+			<div class="gc s1-1 m2-3">
+				<h2>Service Name</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</p>
+				<ul>
+					<li>Detail about the product</li>
+					<li>Detail about the product</li>
+					<li>Detail about the product</li>
+				</ul>
+			</div>
+			<div class="gc s1-1 m1-3">
+				<h3>Pricing</h3>
+				<ul style="list-style: none">
+					<li>XX users - £YY/month</li>
+					<li>XX users - £YY/month</li>
+					<li>XX users - £YY/month</li>
+				</ul>
+			</div>
 		</div>
-		<div class="gc s1-1 l1-2">
-			<h2>Group Co.</h2>
-			<ul>
-				<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-				<li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-				<li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
-			</ul>
+	</div>
+</section>
+
+<section class="service band band--off-white">
+	<div class="container">
+		<div class="grid">
+			<div class="gc s1-1 m2-3">
+				<h2>Service Name</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</p>
+				<ul>
+					<li>Detail about the product</li>
+					<li>Detail about the product</li>
+					<li>Detail about the product</li>
+				</ul>
+			</div>
+			<div class="gc s1-1 m1-3">
+				<h3>Pricing</h3>
+				<ul style="list-style: none">
+					<li>XX users - £YY/month</li>
+					<li>XX users - £YY/month</li>
+					<li>XX users - £YY/month</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="integration band">
+	<div class="container">
+		<h2 class="text-center">Integrations</h2>
+		<div class="grid">
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x50" alt="" class="endorsement-icon"><h3>Company</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x50" alt="" class="endorsement-icon"><h3>Company</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x50" alt="" class="endorsement-icon"><h3>Company</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x50" alt="" class="endorsement-icon"><h3>Company</h3></div>
+			<div class="gc m1-3 l1-5 text-center"><img src="https://via.placeholder.com/150x50" alt="" class="endorsement-icon"><h3>Company</h3></div>
+		</div>
+	</div>
+</section>
+
+<section class="case-studies band">
+	<div class="grid">
+		<div class="gc s1-1 m1-2 gc--vmiddle">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/geomant.png" alt="Geomant">
+		</div>
+		<div class="gc s1-1 m1-2">
+			<h3 class="heading--charlie">Company Name</h3>
+			<h4 class="heading--alpha">+50%</h4>
+			<h3 class="heading--delta">conversions</h3>
 		</div>
 	</div>
 </section>
 
 <section class="cta-banner band band--off-white">
 	<div class="container text-center">
-		<a href="/contact-us/" class="btn btn-primary">Call us</a>
+		<a href="/contact-us/" class="btn btn-primary">Free trial</a>
+		<a href="/contact-us/" class="btn btn-primary">Get in touch</a>
 	</div>
 </section>
