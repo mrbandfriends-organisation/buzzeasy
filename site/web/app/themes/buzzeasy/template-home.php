21bbcc72252df56b97f209dba81ad613ebc8aecd
Anthony Parker <anthonyparker121@gmail.com>
2018-05-01 12:25:38 +0100

    Update callout section to match design

--- site/web/app/themes/buzzeasy/template-home.php
+++ site/web/app/themes/buzzeasy/template-home.php
@@ -118,7 +118,7 @@
 	<div class="container band band--100">
 		<div class="grid">
 			<div class="gc s1-1 l3-4">
-				<h2 class="heading--alpha heading--highlight--white heading--red landmark--half">Catch your customers before the school&nbsp;run?</h2> <br> <br>
+				<h2 class="heading--alpha heading--highlight--white heading--red landmark--half">Catch your <br> customers before <br> the school&nbsp;run?</h2> <br> <br>
 				<a href="/contact-us/" class="btn btn--red btn--fill--white">Free trial</a>
 			</div>
 			<div class="gc s1-1 l1-4">
