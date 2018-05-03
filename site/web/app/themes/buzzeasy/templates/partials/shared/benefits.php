12f72a9f365e83113bde04b074981425a2c97b0e
Anthony Parker <anthonyparker121@gmail.com>
2018-05-03 10:16:16 +0100

    Make benefits section editable

--- site/web/app/themes/buzzeasy/templates/partials/shared/benefits.php
+++ site/web/app/themes/buzzeasy/templates/partials/shared/benefits.php
@@ -4,14 +4,15 @@
  *
  * FIELDS:
  *     benefits
- *     benefits_split (REPEATER)
- *         heading
- *         benefits_list (REPEATER)
- *             benefit
- *
- * MODIFIERS:
- *     heading - 'heading--white'
- *     buttons - 'btn--primary'
+ *         contact_centre_benefits (REPEATER)
+ *             heading
+ *             benefits_list (REPEATER)
+ *                 benefit_item
+ *         marketing_benefits (REPEATER)
+ *             heading
+ *             benefits_list (REPEATER)
+ *                 benefit_item
+ * 
  */
 
 use Roots\Sage\Assets;
@@ -22,34 +23,50 @@ $fields = $fields ?? new ValueCollection();
 
 $modifiers = $modifiers ?? new ValueCollection();
 
-$contact_centre_benefits = $fields->get('contact_centre_benefits');
-$marketing_benefits      = $fields->get('marketing_benefits');
+$contact_centre_benefits      = $fields->get('contact_centre_benefits');
+$contact_centre_benefits_list = $contact_centre_benefits->get('benefits_list');
+$marketing_benefits           = $fields->get('marketing_benefits');
+$marketing_benefits_list      = $marketing_benefits->get('benefits_list');
 
 ?>
 
-<?php if ( $contact_centre_benefits->has() && $marketing_benefits->has() ) : ?>
+<?php if ( $contact_centre_benefits->get('benefits_heading')->has() && $marketing_benefits->get('benefits_heading')->has() ) : ?>
 
     <section class="benefits">
         <div class="container container--extend">
             <div class="grid grid--half-gutter">
 
                     <div class="gc s1-1 l1-2 benefits__contact-centre band band--100 band--blue">
-                        <h2 class="heading--bravo heading--green"></h2>
-                        <ul class="text--green">
-                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-                            <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                            <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
-                        </ul>
+                        <h2 class="heading--bravo heading--green">
+                            <?= $contact_centre_benefits->get('benefits_heading')->escape('html'); ?>
+                        </h2>
+
+                        <?php if( $contact_centre_benefits_list->has() ) : ?>
+                            <ul class="text--green">
+                                <?php foreach( $contact_centre_benefits_list as $benefit ) : ?>
+                                    <li>
+                                        <?= $benefit->get('benefit_item')->escape('html'); ?>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        <?php endif; ?>
                     </div>
 
 
                     <div class="gc s1-1 l1-2 benefits__marketing band band--100 band--green">
-                        <h2 class="heading--bravo heading--blue">Marketing Benefits</h2>
-                        <ul class="text--blue">
-                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-                            <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                            <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
-                        </ul>
+                        <h2 class="heading--bravo heading--blue">
+                            <?= $marketing_benefits->get('benefits_heading')->escape('html'); ?>
+                        </h2>
+
+                        <?php if( $contact_centre_benefits_list->has() ) : ?>
+                            <ul class="text--blue">
+                                <?php foreach( $contact_centre_benefits_list as $benefit ) : ?>
+                                    <li>
+                                        <?= $benefit->get('benefit_item')->escape('html'); ?>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        <?php endif; ?>
                     </div>
             
             </div>
