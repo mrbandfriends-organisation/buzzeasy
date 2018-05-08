500ada58ae0a58972a4a064064c3af9ba5f66689
Anthony Parker <anthonyparker121@gmail.com>
2018-05-08 16:36:55 +0100

    Remove <a> tag from primary logo

--- site/web/app/themes/buzzeasy/templates/partials/site-logo-primary.php
+++ site/web/app/themes/buzzeasy/templates/partials/site-logo-primary.php
@@ -9,11 +9,10 @@
 $modifier = ( !empty( $modifier ) ) ? $modifier : null;
 
 ?>
-<a class="site-logo--primary <?= esc_attr($modifier); ?>" href="<?php echo esc_url( home_url('/') ); ?>">
-	<picture>
-		<!--[if IE 9]><video style="display: none;"><![endif]-->
-		<source type="image/svg+xml" srcset="<?php echo esc_attr( get_stylesheet_directory_uri() . '/assets/svg/standalone/output/logo-primary.svg'); ?>">
-		<!--[if IE 9]></video><![endif]-->
-		<img src="<?php echo esc_attr(get_stylesheet_directory_uri() .'/assets/svg/standalone/output/png/logo-primary.png'); ?>" alt="<?php echo esc_attr(bloginfo('name'));?>">
-	</picture>
-</a>
+
+<picture class="site-logo--primary">
+	<!--[if IE 9]><video style="display: none;"><![endif]-->
+	<source type="image/svg+xml" srcset="<?php echo esc_attr( get_stylesheet_directory_uri() . '/assets/svg/standalone/output/logo-primary.svg'); ?>">
+	<!--[if IE 9]></video><![endif]-->
+	<img src="<?php echo esc_attr(get_stylesheet_directory_uri() .'/assets/svg/standalone/output/png/logo-primary.png'); ?>" alt="<?php echo esc_attr(bloginfo('name'));?>">
+</picture>
