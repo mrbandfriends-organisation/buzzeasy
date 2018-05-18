b99cd1398d40123fd20c7a3315543673d2f7a913
Anthony Parker <anthonyparker121@gmail.com>
2018-05-18 11:37:51 +0100

    Make Testimonials slider CMS editable

--- site/web/app/themes/buzzeasy/templates/components/carousel/testimonials-slider.php
+++ site/web/app/themes/buzzeasy/templates/components/carousel/testimonials-slider.php
@@ -1,46 +1,78 @@
-<section class="case-studies band band--blue">
-	<div class="container">
+<?php
+/**
+ * Component: Testimonials Slider
+ *
+ * FIELDS:
+ *     testimonials (REPEATER)
+ *         testimonial_logo
+ *         testimonial_name
+ *         testimonial_statistic
+ *         testimonial_copy
+ * 
+ */
 
-			<div class="carousel-container">
-				<div class="carousel js-carousel service-carousel">
-					<div class="carousel__slide">
-						<div class="grid">
-							<div class="gc s1-1 m1-2 gc--vmiddle text-center">
-								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/geomant.png" alt="Geomant">
-							</div>
-							<div class="gc s1-1 m1-2">
-								<h3 class="heading--bravo heading--white">Geomant</h3>
-								<h3 class="heading--charlie heading--white">+50% customer satisfaction</h3>
-								<p class="text--white">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
-							</div>
-						</div>
-					</div>
-					<div class="carousel__slide">
-						<div class="grid">
-							<div class="gc s1-1 m1-2 gc--vmiddle text-center">
-								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/geomant.png" alt="Geomant">
-							</div>
-							<div class="gc s1-1 m1-2">
-								<h3 class="heading--bravo heading--white">Geomant</h3>
-								<h3 class="heading--charlie heading--white">+50% customer satisfaction</h3>
-								<p class="text--white">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
-							</div>
-						</div>
-					</div>
-					<div class="carousel__slide">
-						<div class="grid">
-							<div class="gc s1-1 m1-2 gc--vmiddle text-center">
-								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/geomant.png" alt="Geomant">
-							</div>
-							<div class="gc s1-1 m1-2">
-								<h3 class="heading--bravo heading--white">Geomant</h3>
-								<h3 class="heading--charlie heading--white">+50% customer satisfaction</h3>
-								<p class="text--white">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+use Roots\Sage\Assets;
+use Roots\Sage\Utils;
+use Buzzeasy\App\Utilities\ValueCollection;
 
-	</div>
-</section>
+$fields = $fields ?? new ValueCollection();
+
+$modifiers = $modifiers ?? new ValueCollection();
+
+$testimonials = $fields->get('testimonials');
+
+?>
+
+<?php if( $testimonials->has() ) : ?>
+
+    <section class="case-studies band <?= $modifiers->get('section')->escape('attr'); ?>">
+        <div class="container">
+            <div class="carousel-container">
+                <div class="carousel js-carousel testimonial-carousel">
+
+                    <?php foreach($testimonials as $testimonial) : ?>
+
+                        <?php if(  $testimonial->get('testimonial_logo')->has() && $testimonial->get('testimonial_name')->has() && $testimonial->get('testimonial_copy')->has()  ) : ?>
+
+                            <div class="carousel__slide">
+                                <div class="grid grid--vmiddle">
+                                    <div class="gc s1-1 m1-3 gc--vmiddle text-center">
+
+                                        <?php
+                                        $logo_id = $testimonial->get('testimonial_logo')->get('id')->raw();
+                                        echo Assets\lazyload_image($logo_id, [
+                                            'class'   => 'testimonial__logo',
+                                        ]);
+                                        ?>
+
+                                    </div>
+                                    <div class="gc s1-1 m2-3">
+
+                                        <h3 class="heading--bravo heading--white">
+                                            <?= $testimonial->get('testimonial_name')->escape('html'); ?>
+                                        </h3>
+                                        
+                                        <?php if ( $testimonial->get('testimonial_statistic')->has() ) : ?>
+                                            <h4 class="heading--charlie heading--white">
+                                                <?= $testimonial->get('testimonial_statistic')->escape('html'); ?>
+                                            </h4>
+                                        <?php endif; ?>
+
+                                        <p class="text--white">
+                                            <?= $testimonial->get('testimonial_copy')->escape('html'); ?>
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        <?php endif; ?>
+
+                    <?php endforeach; ?>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php endif; ?>
