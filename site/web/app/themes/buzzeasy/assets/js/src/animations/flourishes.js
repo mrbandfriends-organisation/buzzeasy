52c376cfff4dde2cd1ac3a121ad7247e8ffd50f0
Anthony Parker <anthonyparker121@gmail.com>
2018-05-23 16:47:25 +0100

    Update timing, add easing, trigger on inview

--- site/web/app/themes/buzzeasy/assets/js/src/animations/flourishes.js
+++ site/web/app/themes/buzzeasy/assets/js/src/animations/flourishes.js
@@ -2,17 +2,23 @@ import TweenLite from 'gsap/TweenLite';
 import TimeLineLite from 'gsap/TimeLineLite';
 import DrawSVGPlugin from 'third-party/greensock/DrawSVGPlugin';
 
+import inView from 'in-view';
+
 'use strict';
 /**
  * FLOURISHES GREENSOCK
  *
- * The flourish animations
+ * Handles flourish animations when the section is in view
  */
 
-let flourishesTimeline = new TimeLineLite;
+let flourishesTimeline = new TimeLineLite({paused:true});
 
-flourishesTimeline.add( TweenLite.from( '#flourish-1 path', 1.5, {drawSVG:0} ) );
-flourishesTimeline.add( TweenLite.from( '#flourish-2 path', 1.5, {drawSVG:0} ) );
-flourishesTimeline.add( TweenLite.from( '#flourish-3 path', 1.5, {drawSVG:0} ) );
+flourishesTimeline.add( TweenLite.from( '#flourish-1 path', 1.25, {drawSVG:0, ease:Power1.easeIn } ) ); // 154
+flourishesTimeline.add( TweenLite.from( '#flourish-2 path', 1.25, {drawSVG:0, ease:Power0.easeNone } ) ); // 271
+flourishesTimeline.add( TweenLite.from( '#flourish-3 path', 1.25, {drawSVG:0, ease:Power1.easeOut } ) ); // 160
 
-flourishesTimeline.play();
+inView.threshold(0.6);
+
+inView('.customer-journey').on('enter', function(e){
+    flourishesTimeline.play();
+});
