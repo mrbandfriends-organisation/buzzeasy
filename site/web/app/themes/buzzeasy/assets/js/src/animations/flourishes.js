c1b874f1e6bd897af81bf8b4d5f28e1d62c4db69
Anthony Parker <anthonyparker121@gmail.com>
2018-05-24 12:00:19 +0100

    Correct import statements and adjust timings

--- site/web/app/themes/buzzeasy/assets/js/src/animations/flourishes.js
+++ site/web/app/themes/buzzeasy/assets/js/src/animations/flourishes.js
@@ -1,5 +1,5 @@
 import TweenLite from 'gsap/TweenLite';
-import TimeLineLite from 'gsap/TimeLineLite';
+import TimelineLite from 'gsap/TimelineLite';
 import DrawSVGPlugin from 'third-party/greensock/DrawSVGPlugin';
 
 import inView from 'in-view';
@@ -11,14 +11,14 @@ import inView from 'in-view';
  * Handles homepage flourish animations, when the section is in view.
  */
 
-let flourishesTimeline = new TimeLineLite({paused:true});
+let flourishesTimeline = new TimelineLite({paused:true});
 
 flourishesTimeline.add( TweenLite.from( '#flourish-1 path', 1.25, {drawSVG:0, ease:Power1.easeIn } ) );
 flourishesTimeline.add( TweenLite.from( '#flourish-2 path', 1.25, {drawSVG:0, ease:Power0.easeNone } ), '+=0.15' );
 flourishesTimeline.add( TweenLite.from( '#flourish-3 path', 1.25, {drawSVG:0, ease:Power1.easeOut } ), '+=0.15' );
 
 // Trigger the timeline when 60% of the Journey section is in the viewport.
-inView.threshold(0.6);
+inView.threshold(0.5);
 inView('.customer-journey').on('enter', function(e){
     flourishesTimeline.play();
 });
